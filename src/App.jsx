399718a1d15259c4c89346375fe641fc29680e02
Rishi Kumar Singh <rishisingh43210@gmail.com>
2024-02-19 12:19:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <div>Build Blog Application</div>
+    </>
+  )
+}
+
+export default App
